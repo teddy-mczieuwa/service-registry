ecd7cbd0f2a73609e3b9d3015b99fa215a15d58b
teddy <teddymczieuwa@gmail.com>
2020-05-10 01:13:16 +0100

    added bunyan to the start script, fixed a typo in the config file

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ const pjs = require('../package.json')
 
 const {name, version} = pjs
 
-const getLogger = (sericeName, serviceVersion, level) => bunyan.createLogger({
-    name: `${sericeName}:${serviceVersion}`, level
+const getLogger = (serviceName, serviceVersion, level) => bunyan.createLogger({
+    name: `${serviceName}:${serviceVersion}`, level
 })
 
 module.exports = {
